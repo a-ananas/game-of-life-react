205d43e61d4bbcf3973cd7e20c7181fd9dc33968
Antoine <antoine.anastassiades@gmail.com>
2021-09-20 01:06:39 +0200

    cleaning comments

--- src/index.js
+++ src/index.js
@@ -2,38 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-//https://stackoverflow.com/questions/34521797/how-to-add-multiple-classes-to-a-reactjs-component?page=1&tab=votes#tab-top
-// en fait pas besoin, il suffit de concatener un string dans une variable
-//var classNames = require('classnames');
-
-// props veut dire quoi exactement ?
 
 function Cell(props){
-
-/*  var liClassNames = ({
-    'cell': true,
-    'alive': props.value,
-    'dead': !props.value
-  }) */
-
   const classNames = "cell " + (props.value ? 'alive' : 'dead')
   return(
-    // self-closing tag possible ?
-    // Oui en JSX on peut si y a pas d'enfants dans le tag
+    // self-closing tag possible w/ JSX if no children
     <button className={classNames} onClick={props.onClick}/>
   );
 }
-
-//https://fr.reactjs.org/docs/jsx-in-depth.html#functions-as-children
-/*function Repeat(props) {
-  let items = [];
-  const className = props.className;
-  const numTimes = props.numTimes
-  for (let i = 0; i<props.numTimes; i++) {
-    items.push(props.children(i));
-  }
-  return <div className = {className} >{items}</div>;
-}*/
 
 //https://fr.reactjs.org/docs/jsx-in-depth.html#functions-as-children
 class Board extends React.Component {
@@ -55,9 +31,7 @@ class Board extends React.Component {
     for (let rows = 0; rows < boardSize; rows++){
       let cellsInRow = [];
       for (let cols = 0; cols < boardSize; cols++){
-        //don't know if we could do a matrix representation easier
         //should work, return inside a loop don't seem a bad idea
-        //1 return per render ?
         const index = rows.toString()+','+ cols.toString();
         cellsInRow.push(
           this.renderCell(rows, cols, index)
